6b83bdd0aef032b26129276ba42531e802fab05f
tatirusmiati95 <32425968+tatirusmiati95@users.noreply.github.com>
2018-04-10 18:13:37 +0800

    Menggunakan Method set

--- src/praktek03/SandalAksi.java
+++ src/praktek03/SandalAksi.java
@@ -5,9 +5,10 @@ package praktek03;
 public class SandalAksi {
     public static void main(String[] args) {
         Sandal s = new Sandal();
-        s.merk="Crocodol";
-        s.warna="Coklat";
-        s.harga=1000;
+      
+        s.setMerk("Crocodol");
+        s.setWarna("Merah");
+        s.setHarga(10000);
         
         s.cetakInfo();
     }
