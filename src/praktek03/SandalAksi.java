7ebfa6afd532180462ab0f6ae81ff6b9921ebc1f
tatirusmiati95 <32425968+tatirusmiati95@users.noreply.github.com>
2018-04-10 18:06:51 +0800

    Instance buat attribut dan panggil method

--- src/praktek03/SandalAksi.java
+++ src/praktek03/SandalAksi.java
@@ -3,5 +3,12 @@ package praktek03;
 
 
 public class SandalAksi {
-    
+    public static void main(String[] args) {
+        Sandal s = new Sandal();
+        s.merk="Crocodol";
+        s.warna="Coklat";
+        s.harga=1000;
+        
+        s.cetakInfo();
+    }
 }
