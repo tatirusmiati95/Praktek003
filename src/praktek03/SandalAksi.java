f26ca8c3634e7ccddf75f9d26af67e6c253db89c
tatirusmiati95 <32425968+tatirusmiati95@users.noreply.github.com>
2018-04-10 18:17:07 +0800

    Menggunakan method get

--- src/praktek03/SandalAksi.java
+++ src/praktek03/SandalAksi.java
@@ -11,5 +11,15 @@ public class SandalAksi {
         s.setHarga(10000);
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t:");
+        System.out.println(s.getMerk());
+        System.out.print("Warnaknya \t:");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t:");
+        System.out.println(s.getHarga());
+
+
+
     }
 }
