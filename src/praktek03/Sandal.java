3fab37722afe82394781e99c31118e370746afe4
tatirusmiati95 <32425968+tatirusmiati95@users.noreply.github.com>
2018-04-10 18:09:58 +0800

    Buat Getter and Setter

--- src/praktek03/Sandal.java
+++ src/praktek03/Sandal.java
@@ -12,4 +12,30 @@ public class Sandal {
         "Harga\t:"+harga);
    
 }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+     
+     
 }
