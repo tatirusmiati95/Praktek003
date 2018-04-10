7bbb52c23dcfd46211033dba926ce3276e5e1808
tatirusmiati95 <32425968+tatirusmiati95@users.noreply.github.com>
2018-04-10 18:21:49 +0800

    Attribut dan Method

--- src/praktek03/Sepatu.java
+++ src/praktek03/Sepatu.java
@@ -3,5 +3,10 @@ package praktek03;
 
 
 public class Sepatu extends Sandal{
+    private String JenisSepatu;
+    
+    void throttle(){
+        System.out.println("Prok Prok........");
+    }
     
 }
